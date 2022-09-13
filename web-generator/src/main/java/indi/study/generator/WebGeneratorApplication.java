6f41d0b09c902b08fa93d24dac3073f953fa65b8
zhouming <zhou_cming@sina.cn>
2022-09-13 10:20:36 +0800

    添加CodeGenerator模块

---
+++ web-generator/src/main/java/indi/study/generator/WebGeneratorApplication.java
@@ -0,0 +1,13 @@
+package indi.study.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebGeneratorApplication.class, args);
+    }
+
+}
