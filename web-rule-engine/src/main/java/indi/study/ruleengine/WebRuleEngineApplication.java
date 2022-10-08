6f3d6a639bb6b5c11b5d2c8a69f4b3e4556688e2
zhouming <zhou_cming@sina.cn>
2022-10-08 18:03:31 +0800

    规则引擎项目添加

---
+++ web-rule-engine/src/main/java/indi/study/ruleengine/WebRuleEngineApplication.java
@@ -0,0 +1,13 @@
+package indi.study.ruleengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRuleEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRuleEngineApplication.class, args);
+    }
+
+}
